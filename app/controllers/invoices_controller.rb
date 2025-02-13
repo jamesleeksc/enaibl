3ed8bd2dd577df870fe7943d2f5a5bb381aa16ab
jamesleeksc <jameslee.ksc@gmail.com>
2025-02-13 15:11:49 +0900

    load invoices async and only user docs

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,13 +1,28 @@
 class InvoicesController < ApplicationController
-  def index
-    @documents = Document.all
+  before_action :authenticate_user!
 
-    @documents = @documents.where(invoice: true) if params[:invoice] == "true"
+  def index
+    @documents = current_user.all_documents.invoice
+
+    params[:confirmed] ||= "true"
+    params[:shipping] ||= "true"
+    params[:ap_or_ar] ||= "ap"
+
     @documents = @documents.where(confirmed_invoice: true) if params[:confirmed] == "true"
-    @documents = @documents.where(shipping_invoice: true) if params[:shipping] == "true"
+    @documents = @documents.where(shipping_invoice: Utils.to_boolean(params[:shipping])) if params[:shipping].in?(%w[true false])
     @documents = @documents.where(ap_or_ar: params[:ap_or_ar]) if params[:ap_or_ar].in?(%w[ap ar])
+    @documents = @documents.all_with_shipment_number(current_user) if params[:has_shipment_no] == "true"
+
+    if params[:search].present?
+      @documents = @documents.where("invoice_content::text ILIKE :search OR content::text ILIKE :search", search: "%#{params[:search]}%")
+    end
 
     @documents = @documents.order(created_at: :desc)
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream { render partial: 'invoices', locals: { documents: @documents } }
+    end
   end
 
   def show
