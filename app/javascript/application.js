f01664819b210d9a1357bcdeb147c4ff2d0880cb
jamesleeksc <jameslee.ksc@gmail.com>
2025-02-13 15:06:57 +0900

    load invoices async

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
+import LiveFilterController from "./controllers/live_filter_controller"
+const application = Application.start()
+application.register("live-filter", LiveFilterController)
 import "/mdb/js/mdb.umd.min.js"
